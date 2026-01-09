748d09e7d4662de8557c1cbcc9f91a9653807301
Jonedeuf <vmin62340@gmail.com>
2026-01-09 14:51:35 +0100

    Finition Interface Graphique
    Finition du développement de l'interface graphique (corrections de bugs + optimisation)
    
    - Taches restantes : ajouter/modifier des commentaires dans les scripts
    
    NOTE : Ce commit avait été réalisé à la base avant Vendredi 9 Janvier à 15:51. Cependant, suite à une mauvaise manipulation liée à Github Desktop, ce commit a été recrée manuellement sur le site web Github.

---
+++ scripts/erosion.py
@@ -0,0 +1,27 @@
+from modele import Modele
+import matplotlib.pyplot as plt
+
+modele : Modele = Modele('./examples/test_M31_linear.fits')
+
+# --- Affichage des résultats ---
+# Afficher l'image originale, le masque adouci, l'image érodée et le résultat final
+plt.figure(figsize=(20, 5))
+
+plt.subplot(1, 4, 1)
+plt.imshow(modele.getImageOriginale(), cmap='gray', origin='lower')
+plt.title('Image originale')
+
+plt.subplot(1, 4, 2)
+plt.imshow(modele.getMasqueEtoilesAdouci(), cmap='gray', origin='lower')
+plt.title('Masque d\'étoiles adouci')
+
+plt.subplot(1, 4, 3)
+plt.imshow(modele.getImageSansEtoiles(), cmap='gray', origin='lower')
+plt.title('Image sans étoiles')
+
+plt.subplot(1, 4, 4)
+plt.imshow(modele.getImageFinale(), cmap='gray', origin='lower')
+plt.title('Image finale (réduction localisée)')
+
+plt.tight_layout()
+plt.show()
